a53628363f7e16c0c06cdef9c95f38d63aca5854
DESKTOP-LUCCABR\Lucca <brazlucca>
2022-12-14 22:08:21 -0300

    commit

--- src/main/java/com/project/fiapnotes/controller/FiapNotesController.java
+++ src/main/java/com/project/fiapnotes/controller/FiapNotesController.java
@@ -4,10 +4,15 @@ import com.project.fiapnotes.dtos.FiapNotesDto;
 import com.project.fiapnotes.models.FiapNotesModel;
 import com.project.fiapnotes.service.FiapNotesService;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.WritableResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
@@ -18,18 +23,37 @@ import java.util.Optional;
 @CrossOrigin({"http://localhost:3000", "https://viniroxo.github.io/fiap-note/", "https://viniroxo.github.io"})
 public class FiapNotesController {
 
+    @Value("azure-blob://imagem/fiapNotesModel1")
+    private Resource blobFile;
+
     final FiapNotesService service;
 
     public FiapNotesController(FiapNotesService service) {
         this.service = service;
     }
 
+//    @PostMapping
+//    public ResponseEntity<Object> saveNote(@RequestBody FiapNotesDto fiapNotesDto) {
+//        var fiapNotesModel = new FiapNotesModel();
+//        BeanUtils.copyProperties(fiapNotesDto, fiapNotesModel);
+//        fiapNotesModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+//        return ResponseEntity.status(HttpStatus.CREATED).body(service.saveNote(fiapNotesModel));
+//    }
+
     @PostMapping
-    public ResponseEntity<Object> saveNote(@RequestBody FiapNotesDto fiapNotesDto) {
+    public ResponseEntity<Object> saveNote(@RequestBody FiapNotesDto fiapNotesDto, @RequestParam("file") String file) throws IOException {
+
         var fiapNotesModel = new FiapNotesModel();
         BeanUtils.copyProperties(fiapNotesDto, fiapNotesModel);
         fiapNotesModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.saveNote(fiapNotesModel));
+
+        var fiapNotesModel1 = service.saveNote(fiapNotesModel);
+
+        try (OutputStream os = ((WritableResource) this.blobFile).getOutputStream()) {
+            os.write(file.getBytes());
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(fiapNotesModel1);
     }
 
     @GetMapping
