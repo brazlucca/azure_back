6313941b9b9049252fa3f48f0f183614632f89d2
DESKTOP-LUCCABR\Lucca <brazlucca>
2022-12-15 20:01:00 -0300

    Banco de dados e blob funcional, ajustar para receber vários paràmetros

--- src/main/java/com/project/fiapnotes/controller/FiapNotesController.java
+++ src/main/java/com/project/fiapnotes/controller/FiapNotesController.java
@@ -3,16 +3,22 @@ package com.project.fiapnotes.controller;
 import com.project.fiapnotes.dtos.FiapNotesDto;
 import com.project.fiapnotes.models.FiapNotesModel;
 import com.project.fiapnotes.service.FiapNotesService;
+import com.project.fiapnotes.service.FileService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.WritableResource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
@@ -23,38 +29,48 @@ import java.util.Optional;
 @CrossOrigin({"http://localhost:3000", "https://viniroxo.github.io/fiap-note/", "https://viniroxo.github.io"})
 public class FiapNotesController {
 
-    @Value("azure-blob://imagem/fiapNotesModel1")
-    private Resource blobFile;
 
     final FiapNotesService service;
 
-    public FiapNotesController(FiapNotesService service) {
+    public FiapNotesController(FiapNotesService service, FileService fileService) {
         this.service = service;
+        this.fileService = fileService;
     }
 
-    @PostMapping
-    public ResponseEntity<Object> saveNote(@RequestBody FiapNotesDto fiapNotesDto) {
-        var fiapNotesModel = new FiapNotesModel();
-        BeanUtils.copyProperties(fiapNotesDto, fiapNotesModel);
-        fiapNotesModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.saveNote(fiapNotesModel));
-    }
+    private final FileService fileService;
 
 //    @PostMapping
-//    public ResponseEntity<Object> saveNote(@RequestBody FiapNotesDto fiapNotesDto, @RequestParam("file") String file) throws IOException {
-//
+//    public ResponseEntity<Object> saveNote(@RequestBody FiapNotesDto fiapNotesDto) {
 //        var fiapNotesModel = new FiapNotesModel();
 //        BeanUtils.copyProperties(fiapNotesDto, fiapNotesModel);
 //        fiapNotesModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
-//
-//        var fiapNotesModel1 = service.saveNote(fiapNotesModel);
-//
-//        try (OutputStream os = ((WritableResource) this.blobFile).getOutputStream()) {
-//            os.write(file.getBytes());
-//        }
-//
-//        return ResponseEntity.status(HttpStatus.CREATED).body(fiapNotesModel1);
+//        return ResponseEntity.status(HttpStatus.CREATED).body(service.saveNote(fiapNotesModel));
 //    }
+
+    @PostMapping
+    public ResponseEntity<Object> saveNote(@RequestBody MultipartFile file) throws IOException {
+
+        var fiapNotesModel = new FiapNotesModel();
+        fiapNotesModel.setText("Texto Teste");
+        fiapNotesModel.setUrgent(true);
+//        BeanUtils.copyProperties(fiapNotesDto, fiapNotesModel);
+        fiapNotesModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+
+        var fiapNotesModel1 = service.saveNote(fiapNotesModel);
+
+        try {
+            if (fileService.uploadAndDownloadFile(file, "imagem", "img" + fiapNotesModel1.getId().toString())) {
+                final ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(Paths.get(fileService
+                        .getFileStorageLocation() + "/" + file.getOriginalFilename())));
+                return ResponseEntity.status(HttpStatus.OK).contentLength(resource.contentLength()).body(resource);
+            }
+            //return ResponseEntity.ok("Error while processing file");
+        } catch (Exception e) {
+            // return ResponseEntity.ok("Error while processing file");
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(fiapNotesModel1);
+    }
 
     @GetMapping
     public ResponseEntity<List<FiapNotesModel>> getNotes() {
